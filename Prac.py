6c00ddf38bc9e6d332f23d2eb5a1c4586c5f698f
user.email <geoffreykioi7@gmail.com>
2024-08-01 15:41:55 +0300

    Latest commit

---
+++ Prac.py
@@ -0,0 +1,5 @@
+list = [3,4,5,6,7,8,9,1,2]
+
+first = list.pop(3)
+
+print(first)
--- classes.py
+++ classes.py
@@ -11,7 +11,7 @@ class Employee:
         Employee.num_of_emps += 1
 
     def fullname(self):
-        return '{riest} {last}'
+        return '{first} {last}'
     
     
     def apply_raise(self):
